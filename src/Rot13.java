7c15d6d489e5fda338fd30638ad3714092d5e351
OSAMA <2023_osama.belaatar@iticbcn.cat>
2025-09-25 19:52:40 +0200

    gurdant excercici rot13 part 1

---
+++ src/Rot13.java
@@ -0,0 +1,28 @@
+
+public class Rot13 {
+
+    static final String abc = "aàábcçdeèééfghiíjklmnoòópqrstuúüvwxyzç";
+    static final String ABC = "AÀÁBCÇDEÈÉÉFGHIÍJKLMNOÒÓPQRSTUÚüVWXYZÇ";
+
+        char [] abcArray = abc.toCharArray();
+        char [] ABCArray = abc.toCharArray();
+    
+
+    
+    public String xifraRot13 (String text){
+        return "o";
+    }
+
+    public String desxifraRot13 (String text){
+        return "o";
+    }
+
+
+
+    public static void main(String[] args) {
+
+    
+
+    
+    }
+}
